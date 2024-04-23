32c240bdd9f7a16453d8d97dff8c13b95b537ada
Baiju Joseph <baiju.joseph@gmail.com>
2024-04-23 23:46:59 +0100

    PP-1: feat - adding first wordings and bg to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      Home Page
+    <main className='w-screen h-screen relative'>
+      <div className='flex items-center w-full h-full bg-cover bg-center' style={{backgroundImage: "url(/main-bg.webp)"}}>
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+          <h1 className="text-[50px] text-white font-bold font-italic">
+            BAIJU JOSEPH  <br />
+            <span className="text-transparent text-bold bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              Full-Stack Developer, <br/>AWS Cloud Practitioner & <br/>Certified Developer
+            </span>
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
